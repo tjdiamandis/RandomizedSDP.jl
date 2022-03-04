08639cc6a3b5ece430c1af728343d52ea0fcc4bf
Theo Diamandis <tjdiamandis@gmail.com>
2022-03-03 23:46:10 -0800

    small test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using RandomizedSDP
+using LinearAlgebra
 using Test
 
+const RS = RandomizedSDP
+
 @testset "RandomizedSDP.jl" begin
-    # Write your tests here.
+    n = 100
+    for i in 1:5
+        b, F, G, xstar, _ = RS.generate_random_sdp(n; rand_seed=i)
+        pstar = dot(b, xstar)
+
+        ## Real Problem
+        c = Vector(RS.vec_symm(-G))
+        A = RS.build_A(F)
+        sdp = RS.SDP(c, A, b)
+        result = RS.solve!(
+            sdp; 
+            relax=false, 
+            print_iter=10, 
+            indirect=true, 
+            precondition=true, 
+            logging=true, 
+            verbose=false
+        )
+        @test abs(sdp.obj_val - pstar) / min(sdp.obj_val, pstar) <= 1e-6
+    end
 end
